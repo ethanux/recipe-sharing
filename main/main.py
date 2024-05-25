b38b41531b5de5d6d77459a4943ef2533c65418e
Adivhaho Jeff <221649921@edu.vut.ac.za>
2024-05-25 23:29:21 +0200

    added add_ingrediants route

--- main/main.py
+++ main/main.py
@@ -16,9 +16,27 @@ def add_recipe():
 		instructions = request.form['instructions']
 		category = request.form['category']
 		image = request.form['images']
-
 		recipe = Recipe(title=title, description=description, instructions=instructions, category=category, image=image, user_id=current_user.id)
 		db.session.add(recipe)
 		db.session.commit()
-	
+		flash("recipe added successfully ", 'success')
+		return "add success recipe "
 	return "add recipe"
+
+
+@main.route('/add-ingrediants', methods=['POST', 'GET'])
+def add_ingrediants():
+	if not current_user.is_authenticated:
+		flash('Logg in first to visit page', 'warning')
+		return redirect(url_for('auth.login'))
+
+	if requst.method == "POST":
+		item = request.form['title']
+		amount = request.form['description']
+		recipe_id = request.form['recipe_id']
+		ingrediants = Ingrediants(item=item, amount=amount,recipe_id=recipe_id)
+		db.session.add(ingrediants)
+		db.session.commit()
+		flash("ingrediants added successfully ", 'success')
+		return "add success ingrediants "
+	return "add ingrediants"
