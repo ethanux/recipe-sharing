903bdeb4f7a419ef9702c99cd642e53017e158c3
Adivhaho Jeff <221649921@edu.vut.ac.za>
2024-05-25 22:26:18 +0200

    added table Recipe,Ingrediants and Comment to the database

--- models/model.py
+++ models/model.py
@@ -1,34 +1,46 @@
-# Import the 'db' instance from the 'ext' module
+
 from ext.ext import db
 
-# Import the 'datetime' module to work with timestamps
 from datetime import datetime
 
-# Import 'UserMixin' from Flask-Login for user authentication features
 from flask_login import UserMixin
 
-# Define a class named 'User' representing a user in the application
 class User(UserMixin, db.Model):
-    """
-    Represents a user in the application.
-
-    Inherits from UserMixin for user authentication features provided by Flask-Login.
-    Inherits from db.Model to define the database model using SQLAlchemy ORM.
-
-    Attributes:
-    - id (int): Primary key for the user record.
-    - fullname (str): Full name of the user.
-    - username (str): username address of the user.
-    - password (str): Password of the user.
-    - timestamp (datetime): Timestamp indicating the creation date of the user record.
-    """
-    # Define a column 'id' of type Integer as the primary key
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    # Define a column 'username' of type String with a maximum length of 50 characters
     username = db.Column(db.String(50), nullable=False)
-
-    # Define a column 'password' of type String with a maximum length of 50 characters
     password = db.Column(db.String(50), nullable=False)
-
-    # Define a column 'timestamp' of type DateTime with a default value of the current timestamp
     timestamp = db.Column(db.DateTime, nullable=True, default=datetime.now())
+    recipes = db.relationship('Recipe', backref='user', cascade="all, delete-orphan", lazy=True)
+
+
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    instructions = db.Column(db.String(225), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    submission_date = db.Column(db.DateTime, nullable=True, default=datetime.now())
+    approval_status = db.Column(db.Boolean, nullable=False, default=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    ingredients = db.relationship('Ingrediants', backref='recipe', cascade="all, delete-orphan", lazy=True)
+
+class Ingrediants(db.Model):
+    __tablename__ = "ingredients"
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(50), nullable=False)
+    ammount = db.Column(db.String(50), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=True, default=datetime.now())
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
